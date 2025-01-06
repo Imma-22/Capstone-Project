02117eaf37b97818b0b63d9f412ea71b862b03de
Imma <immaculata222@gmail.com>
2025-01-07 00:52:19 +0100

    Updated file

--- weather-dashboard/src/components/Footer.jsx
+++ weather-dashboard/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="pt-8 text-gray-500 text-center font-extrabold text-xs w-full ">
+        <footer className="pt-8 text-gray-400 text-center font-extrabold text-sm w-full ">
             <p>&copy; {new Date().getFullYear()} Weather Dashboard.</p>
             <p>
                 Data provided by{' '}
